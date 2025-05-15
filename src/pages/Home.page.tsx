f02961f246852e69da50dbe0022614b509f49289
SD2711 <126335400+SD2711@users.noreply.github.com>
2025-05-15 09:27:25 +0300

    add header

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,11 @@
+import { HeaderSimple } from '@/components/Header/HeaderSimple';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export function HomePage() {
   return (
     <>
+    <HeaderSimple/>
       <Welcome />
       <ColorSchemeToggle />
     </>
